209bfcc861c8c0220601c887aa082002e25450a0
Daniyal Ali Israil <daniyalali12568@gmail.com>
2025-07-11 03:31:52 -0700

    array

--- class_13_array.cs
+++ class_13_array.cs
@@ -11,8 +11,29 @@ class LearnArray
         Console.WriteLine($"Array elements:{num[0]}, {num[1]}, {num[2]}");
         Console.WriteLine("Length of array: " + num.Length);
         Console.WriteLine("Array type: " + num.GetType());
-        Console.WriteLine("Array rank: " + num.Rank);
-        // Console.WriteLine("Array is of type: " + num.GetType().Name);
-        // Console.WriteLine("Array is of type: " + num.GetType().FullName);
+        Console.WriteLine("Array rank: " + num.Rank);//rank define the dimenion of the array
+
+        //loop through the array using for loop and foreach loop
+        // for (int i = 0; i < num.Length; i++)
+        // {
+        //     Console.WriteLine($"Element at index {i}: {num[i]}");
+        // }
+
+        foreach (int item in num)
+        {
+            Console.WriteLine($"Element in foreach loop: {item}");
+        }
+        //sum of numbers in an array
+        int[] numarr = new int[3];
+        int sum = 0;
+        for (int i = 0; i < numarr.Length; i++)
+        {
+            Console.WriteLine($"Enter a Number");
+            int usernumber = Convert.ToInt32(Console.ReadLine());
+            sum += usernumber;
+        }
+        Console.WriteLine($"Sum of the numbers: {sum}");
+        Console.WriteLine(sum == 90 ? "valid" : "invalid");
+
     }
 }
